3255c29bc96696b2cd7601e961441c1eb16b24a5
MabroukNADSoft <ahmad.mabrouk@nadsoft.net>
2024-05-21 15:19:58 +0300

    allow accepting and cleaning arabic letters

--- codes/models/audio/tts/tacotron2/text/cleaners.py
+++ codes/models/audio/tts/tacotron2/text/cleaners.py
@@ -1,6 +1,6 @@
 """ from https://github.com/keithito/tacotron """
 
-'''
+"""
 Cleaners are transformations that run over the input text at both training and eval time.
 
 Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
@@ -10,82 +10,70 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
      the Unidecode library (https://pypi.python.org/pypi/Unidecode)
   3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
      the symbols in symbols.py to match your data).
-'''
+"""
 
 import re
 from unidecode import unidecode
 from .numbers import normalize_numbers
 
+from phonemizer.backend import EspeakBackend
+from phonemizer.punctuation import Punctuation
+from phonemizer.separator import Separator
+
+from pylibtashkeel import tashkeel
 
 # Regular expression matching whitespace:
-_whitespace_re = re.compile(r'\s+')
-
-# List of (regular expression, replacement) pairs for abbreviations:
-_abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
-]]
-
-
-def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
+_whitespace_re = re.compile(r"\s+")
 
 
 def expand_numbers(text):
-  return normalize_numbers(text)
+    return normalize_numbers(text)
 
 
 def lowercase(text):
-  return text.lower()
+    return text.lower()
 
 
 def collapse_whitespace(text):
-  return re.sub(_whitespace_re, ' ', text)
+    return re.sub(_whitespace_re, " ", text)
 
 
 def convert_to_ascii(text):
-  return unidecode(text)
+    return unidecode(text)
 
 
 def basic_cleaners(text):
-  '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
+    """Basic pipeline that lowercases and collapses whitespace without transliteration."""
+    text = lowercase(text)
+    text = collapse_whitespace(text)
+    return text
 
 
 def transliteration_cleaners(text):
-  '''Pipeline for non-English text that transliterates to ASCII.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
+    """Pipeline for non-English text that transliterates to ASCII."""
+    text = convert_to_ascii(text)
+    text = lowercase(text)
+    text = collapse_whitespace(text)
+    return text
+
+
+def phonemic_transcription(text):
+    text = Punctuation(';:,.!"?()-').remove(text)
+    words = text.split()  # split the text into a list of words
+    backend = EspeakBackend("ar")
+    separator = Separator(phone=" ", word=None)
+    phonemes = backend.phonemize(
+        words, separator=separator
+    )  # pass the separator to phonemize
+    return phonemes
 
 
 def english_cleaners(text):
-  '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
-  text = collapse_whitespace(text)
-  text = text.replace('"', '')
-  return text
+    """Pipeline for English text, including number and abbreviation expansion."""
+    text = tashkeel(text)
+    text = phonemic_transcription(text)
+    text = lowercase(text)
+    text = expand_numbers(text)
+    text = collapse_whitespace(text)
+    text = text.replace('"', "")
+    return text
